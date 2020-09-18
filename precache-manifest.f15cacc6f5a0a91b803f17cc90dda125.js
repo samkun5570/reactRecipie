d2413f5deecf3e3ce1a1db3385031a9bc5774ca3
samkun5570 <samirdey5570@gmail.com>
2020-09-18 23:34:14 +0530

    Updates

---
+++ precache-manifest.f15cacc6f5a0a91b803f17cc90dda125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6e21126d3171173623e0c4f69a98c0",
+    "url": "/samkun5570/reactRecipie/index.html"
+  },
+  {
+    "revision": "6ffd4705a01c56978bb1",
+    "url": "/samkun5570/reactRecipie/static/css/main.395aaa17.chunk.css"
+  },
+  {
+    "revision": "2f93e0587846f618cb6b",
+    "url": "/samkun5570/reactRecipie/static/js/2.1ab8b3e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samkun5570/reactRecipie/static/js/2.1ab8b3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ffd4705a01c56978bb1",
+    "url": "/samkun5570/reactRecipie/static/js/main.3be64b93.chunk.js"
+  },
+  {
+    "revision": "d7b5ac1c5af2d078afbe",
+    "url": "/samkun5570/reactRecipie/static/js/runtime-main.2cf14e32.js"
+  }
+]);
