5518deab9cac68ed0790fbd62f74f1bf5200b6c2
samkun5570 <samirdey5570@gmail.com>
2020-09-27 21:47:19 +0530

    tilt added

--- src/recepie.js
+++ src/recepie.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
 // import Style from './recepie.css'
 import './recepie.css'
 const recepie = ({title,calories,img,ingredients}) =>{
     return(
         <article className="recepie">
             <header className="card-header">{title}</header>
+            <Tilt>
             <img src = {img} alt=" " className="imgClass"></img>
+            </Tilt>
             <p>Calories : {calories.toFixed(0)}</p>
             <ol className="ingrediants">
                 {
